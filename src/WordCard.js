7515801a544994c2d30a26d7d82b661a84afb78d
Thanet <6010110557@psu.ac.th>
2019-09-22 03:53:41 +0700

    add playing rules

--- src/WordCard.js
+++ src/WordCard.js
@@ -44,9 +44,10 @@ export default class WordCard extends Component{
                 <h1>The name of this lap is</h1>
                 {Array.from(this.state.chars).map(( c , i ) => <CharacterCard value = {c} key = {i} attempt= {this.state.attempt} activationHandler={this.activationHandler}></CharacterCard>)}
                 <h1>---->{this.state.guess}</h1>
-                <h2>{this.state.completed ? 'YOU WIN': '.'}</h2>
-                <h2>Your Round = {this.state.attempt}</h2>
-                
+                <h2>Your Round = {this.state.attempt} / 5 </h2>
+                <h2>{this.state.completed ? 'YOU WIN': ''}</h2>
+                <h2>{this.state.attempt == 6? 'YOU LOSE             ***ans is REACTLAB***':''}</h2>
+
             </div>
             
         );
