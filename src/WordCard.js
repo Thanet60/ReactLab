1c5ecba15813e7516fcc6776b33ee73a3e354583
Thanet <6010110557@psu.ac.th>
2019-09-22 03:40:56 +0700

    change color

--- src/WordCard.js
+++ src/WordCard.js
@@ -43,7 +43,10 @@ export default class WordCard extends Component{
             <div>
                 <h1>The name of this lap is</h1>
                 {Array.from(this.state.chars).map(( c , i ) => <CharacterCard value = {c} key = {i} attempt= {this.state.attempt} activationHandler={this.activationHandler}></CharacterCard>)}
+                <h1>---->{this.state.guess}</h1>
+                <h2>{this.state.completed ? 'YOU WIN': '.'}</h2>
                 <h2>Your Round = {this.state.attempt}</h2>
+                
             </div>
             
         );
